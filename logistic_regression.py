dde0564a96bd3dd22e01ca6a402607e06e2aa121
Quocviet Le <quocvietlet@gmail.com>
2024-09-08 14:00:16 -0500

    load different dataset

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # load dataset
-dataset = pd.read_csv("Social_Network.csv")
+dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
