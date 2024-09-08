a6339adbcd9bfc0e23da38dc8e8921730003ba50
Quocviet Le <quocvietlet@gmail.com>
2024-09-08 13:59:29 -0500

    load dataset, split training and testing

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,3 +4,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# import ML libaries
+from sklearn.model_selection import train_test_split
+
+# load dataset
+dataset = pd.read_csv("Social_Network.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# splitting the dataset into the training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
