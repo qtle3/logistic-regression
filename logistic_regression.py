c7f925d310618101c07d3c1ec7a77ee624d1180f
Quocviet Le <quocvietlet@gmail.com>
2024-09-08 14:06:48 -0500

    feature scaling

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 # import ML libaries
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # load dataset
 dataset = pd.read_csv("Social_Network_Ads.csv")
@@ -14,4 +15,11 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # splitting the dataset into the training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0
+)
+
+# feature scaling
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.fit_transform(X_test[:, 3:])
