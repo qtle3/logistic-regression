0c7dac80eafc6ad6cee023b58a4d95be1e3d823e
Quocviet Le <quocvietlet@gmail.com>
2024-09-08 20:01:11 -0500

    import logistic regression and train model

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,20 +8,24 @@ import matplotlib.pyplot as plt
 # import ML libaries
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 # load dataset
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+
 # splitting the dataset into the training set and test set
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=0
 )
 
-# feature scaling
+# feature scaling the data
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
-#
+# training logistic regression model
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
