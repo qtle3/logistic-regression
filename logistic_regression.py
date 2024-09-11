9b54cc13a2ed86481b825c6ce67c0b2df37b51b3
Quocviet Le <quocvietlet@gmail.com>
2024-09-10 19:50:22 -0500

    plot both test and training

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
 
 # load dataset
 dataset = pd.read_csv("Social_Network_Ads.csv")
@@ -45,3 +46,63 @@ print(
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 print(accuracy_score(y_test, y_pred))
+
+# visualize training set results
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
+    np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.25),
+)
+plt.contourf(
+    X1,
+    X2,
+    classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(
+        X1.shape
+    ),
+    alpha=0.75,
+    cmap=ListedColormap(("salmon", "dodgerblue")),
+)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0],
+        X_set[y_set == j, 1],
+        c=ListedColormap(("salmon", "dodgerblue"))(i),
+        label=j,
+    )
+plt.title("Logistic Regression (Training set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+
+# visualize test set results
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
+    np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.25),
+)
+plt.contourf(
+    X1,
+    X2,
+    classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(
+        X1.shape
+    ),
+    alpha=0.75,
+    cmap=ListedColormap(("salmon", "dodgerblue")),
+)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0],
+        X_set[y_set == j, 1],
+        c=ListedColormap(("salmon", "dodgerblue"))(i),
+        label=j,
+    )
+plt.title("Logistic Regression (Test set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
